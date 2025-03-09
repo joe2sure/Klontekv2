dcbea82f61ba0ec58a0962126d6b6a33a6c65a50
unknown <joe2sure1@gmail.com>
2025-03-09 11:07:35 +0100

    completed the hompage ServiceSlidesSection section

--- src/components/Element/Cards/ServiceCard.jsx
+++ src/components/Element/Cards/ServiceCard.jsx
@@ -18,24 +18,3 @@ const ServicesCard = ({ item }) => {
 };
 
 export default ServicesCard;
-
-
-// import Link from "next/link";
-
-// const ServiceCard = ({ item }) => {
-//     return (
-//         <div className="tekup-iconbox-wrap border-all">
-//             <div className="tekup-iconbox-icon">
-//                 <img src={item?.icon} alt="" />
-//             </div>
-//             <div className="tekup-iconbox-data">
-//                 <Link href="single-service">
-//                     <h4>{item?.title}</h4>
-//                 </Link>
-//                 <p>{item?.description}</p>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default ServiceCard;
