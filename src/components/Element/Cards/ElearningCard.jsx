57b473b7f15f36b6ab9f4f774b62588112f8736e
unknown <joe2sure1@gmail.com>
2025-03-13 21:12:27 +0100

    completed the home page

--- src/components/Element/Cards/ElearningCard.jsx
+++ src/components/Element/Cards/ElearningCard.jsx
@@ -4,17 +4,49 @@ const ElearningCard = ({ item }) => {
     return (
         <div className="tekup-portfolio-wrap">
             <div className="tekup-portfolio-thumb">
-                <img src={item?.image} alt="" />
+                {/* Add inline styles or a class to enforce image dimensions */}
+                <img 
+                    src={item?.image} 
+                    alt="" 
+                    style={{ 
+                        width: '416px', 
+                        height: '450px', 
+                        objectFit: 'cover' 
+                    }} 
+                />
                 <div className="tekup-portfolio-data">
                     <Link href="single-portfolio">
                         <h5>{item?.title}</h5>
                     </Link>
                     <p>{item?.category}</p>
-                    <Link className="tekup-portfolio-btn" href="single-portfolio"><i className="ri-arrow-right-up-line"></i></Link>
+                    <Link className="tekup-portfolio-btn" href="single-portfolio">
+                        <i className="ri-arrow-right-up-line"></i>
+                    </Link>
                 </div>
             </div>
         </div>
     );
 };
-
 export default ElearningCard;
+
+
+// import Link from "next/link";
+
+// const ElearningCard = ({ item }) => {
+//     return (
+//         <div className="tekup-portfolio-wrap">
+//             <div className="tekup-portfolio-thumb">
+//                 <img src={item?.image} alt="" />
+//                 <div className="tekup-portfolio-data">
+//                     <Link href="single-portfolio">
+//                         <h5>{item?.title}</h5>
+//                     </Link>
+//                     <p>{item?.category}</p>
+//                     <Link className="tekup-portfolio-btn" href="single-portfolio"><i className="ri-arrow-right-up-line"></i></Link>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ElearningCard;
